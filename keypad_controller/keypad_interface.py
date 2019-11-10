6f0402d2e7d3a5c833def3da5b4af423f788c955
Philip <pnaidacarleton@gmail.com>
2019-11-09 23:01:49 -0500

    added functions to be able to read credentials from keypad

--- keypad_controller/keypad_interface.py
+++ keypad_controller/keypad_interface.py
@@ -1,14 +1,44 @@
+from hashlib import sha256
+from keypad_library import keypad
+from time import sleep
+
+
+kp = keypad()
 
 
 def read_keypad():
     '''Will read user inputs from the keypad and save them in a Python tuple. 
     The tuple format is (user_code, hashed_passcode, safe number)'''
-    return ("", "", "")
+    
+    keys = []
+    
+    # Get 9 key presses: 4 digit user code, 4 digit passcode, 1 digit safe number.
+    for i in range(9):
+        keypress = get_digit()
+        print(keypress)
+        keys.append(keypress)
+        sleep(0.5)
+    
+    user_code = str(keys[0]) + str(keys[1]) + str(keys[2] + str(keys[3])
+    passcode = str(keys[4]) + str(keys[5]) + str(keys[6]) + str(keys[7])
+    safe_number = str(keys[8])
+
+    return (user_code, passcode, safe_number)
+
+
+def get_digit():
+    # poll for key press
+    key = None
+    while key == None:
+        key = kp.getKey()
+    return key
 
 
 def hash_passcode(passcode):
     '''Will hash the user entered passcode using SHA256 hashing algorithm.'''
-    return ""
+    hash_object = sha256(passcode.encode())
+    hash_digest = hash_object.hexdigest()
+    return hash_digest
 
 
 def set_LED(success):
@@ -25,9 +55,10 @@ def receive_udp_ack():
     return False
 
 
-def verify_user_credentials():
+def verify_user_credentials(user_credentials):
     '''Verify user credentials match with expected format for credentials.'''
     return False
 
 
+hash_passcode('hello')
 
