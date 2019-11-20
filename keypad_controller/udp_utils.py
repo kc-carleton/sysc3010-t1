153a0433f57a734ff0d71bd2ac47cd9de8aa4122
Philip <philipnaida@cmail.carleton.ca>
2019-11-20 17:39:25 -0500

    fixed udp utils file to match design document and fixed udp utils tests (they are all working now)

--- keypad_controller/udp_utils.py
+++ keypad_controller/udp_utils.py
@@ -1,7 +1,7 @@
 from enum import Enum
 import socket
 import json
-
+import select
 
 def send_pkt(data, ip_address, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,8 +13,14 @@ def receive_pkt(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = ('', port)
     s.bind(server_address)
-    buf, address = s.recvfrom(port)
-    return (buf, address)
+
+    s.setblocking(0)
+    timeout_length = 60
+    ready = select.select([s], [], [], timeout_length)
+    if ready[0]:
+        data, address = s.recvfrom(port)
+        return data, address
+    return None, 'socket timed out'
 
 
 def create_data(json_data):
@@ -23,7 +29,7 @@ def create_data(json_data):
     RETURN a byte array representing the data in the DATA packet to be sent.'''
     if json_data is None:
         return None
-    data = '{}\0{}\0'.format(PacketType.DATA.value, str(json_data))
+    data = '{}{}\0'.format(PacketType.DATA.value, str(json_data))
     return data.encode()
 
 
@@ -33,27 +39,33 @@ def decode_data(encoded):
     RETURN a string representing the data which was sent in the DATA packet.'''
     if encoded is None:
         return None, 'empty_packet'
+    if len(encoded) == 0:
+        return None, 'empty_byte_array'
     if encoded.decode('utf-8')[-1] != '\0':
         return None, 'no_null_termination'
-    data = encoded.decode('utf-8').split('\0')
-    if data[0] != PacketType.DATA.value:
+   
+    data_string = encoded.decode('utf-8')
+    opcode = data_string[:4]
+    data = data_string[4:].split('\0')[0]
+
+    if opcode != PacketType.DATA.value:
         return None, 'incorrect_data_opcode'
-    if data[1] == '':
+    if data == '':
         return None, 'no_json_data'
     try:
-        json_data = json.loads(data[1])
+        json_data = json.loads(data)
         keys = json_data.keys()
         if len(keys) != 3:
             raise Exception("err")
-        if 'user_name' not in keys or 'hashed_passcode' not in keys or 'safe' not in keys:
+        if 'user_code' not in keys or 'hashed_passcode' not in keys or 'safe_number' not in keys:
             raise Exception("err")
     except:
         return None, 'incorrect_json'
-    return data[1], ''
+    return data, ''
 
 
-def create_ack(ack_type):
-    data = '{}\0{}\0'.format(PacketType.ACK.value, str(ack_type))
+def create_ack(success):
+    data = '{}{}\0'.format(PacketType.ACK.value, str(success))
     return data.encode()
 
 
@@ -62,34 +74,46 @@ def decode_ack(encoded):
         return None, 'empty_packet'
     if encoded.decode('utf-8')[-1] != '\0':
         return None, 'no_null_termination'
-    data = encoded.decode('utf-8').split('\0')
-    if data[0] != PacketType.ACK.value:
+   
+    data_string = encoded.decode('utf-8')
+    opcode = data_string[:4]
+    data = data_string[4:].split('\0')[0]
+
+    if opcode != PacketType.ACK.value:
         return None, 'incorrect_ack_opcode'
-    if len(data[1]) == 0:
+    if len(data) == 0:
         return None, 'no_command'
-    if len(data[1]) != 1:
+    if data != 'True' and data != 'False':
         return None, 'inavlid_command'
-    return data[1] ,''
+    return bool(data) ,''
 
 
 def create_command(cmd):
     if cmd is None:
-        return None
-    data = '{}\0{}\0'.format(PacketType.COMMAND.value, str(cmd))
-    return data.encode()
+        return None, 'empty_command'
+    if cmd is '':
+        return None, 'empty_command'
+    data = '{}{}\0'.format(PacketType.COMMAND.value, str(cmd))
+    return data.encode(), ''
 
 
 def decode_command(encoded):
     if encoded is None:
         return None, 'empty_packet'
-    data = encoded.decode('utf-8').split('\0')
-    if data[0] != PacketType.COMMAND.value:
+    if len(encoded) == 0:
+        return None, 'empty_packet'
+    if encoded.decode('utf-8')[-1] != '\0':
+        return None, 'no_null_termination'
+
+    data_string = encoded.decode('utf-8')
+    opcode = data_string[:4]
+    data = data_string[4:].split('\0')[0]
+    
+    if opcode != PacketType.COMMAND.value:
         return None, 'Not a COMMAND packet'
-    if len(data) != 3:
-        return None, 'byte array format is incorrect'
-    if len(data[1]) == 0:
+    if len(data) == 0:
         return None, 'Command field is empty'
-    return data[1], ''
+    return data, ''
 
 
 def create_error(error_message):
@@ -97,19 +121,27 @@ def create_error(error_message):
         return None
     elif len(error_message) == 0:
         return None
-    data = '{}\0{}\0'.format(PacketType.ERROR.value, str(error_message))
+    data = '{}{}\0'.format(PacketType.ERROR.value, str(error_message))
     return data.encode()
 
 
 def decode_error(encoded):
     if encoded is None:
         return None, 'empty_packet'
-    data = encoded.decode('utf-8').split('\0')
-    if data[0] != PacketType.ERROR.value:
+    if len(encoded) is 0:
+        return None, 'empty_packet'
+    if encoded.decode('utf-8')[-1] != '\0':
+        return None, 'no_null_termination'
+
+    data_string = encoded.decode('utf-8')
+    opcode = data_string[:4]
+    data = data_string[4:].split('\0')[0]
+    
+    if opcode != PacketType.ERROR.value:
         return None, 'Not an ERROR packet'
-    if len(data) != 3:
-        return None, 'byte array format is incorrect'
-    return data[1], ''
+    if len(data) == 0:
+        return None, 'Empty error message'
+    return data, ''
 
 
 class PacketType(Enum):
